157ffb5beeed708c41bc7644801162080df14d3f
Caterine Studzinski <89097644+Nekido@users.noreply.github.com>
2023-03-03 22:02:15 -0300

    Criação classe cliente.

---
+++ src/main/java/br/com/estacionamento/CFJ/Cliente/Cliente.java
@@ -0,0 +1,68 @@
+package br.com.estacionamento.CFJ.Cliente;
+
+package com.empresa.estacionamento.cliente;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String CPF;
+    private String telefone;
+    private String email;
+
+    public Cliente() {}
+
+    public Cliente(String nome,String CPF, String telefone, String email) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
